bc75a631fa0dcc32ad8e87a03d560a0d2c972b94
Pedro Lucas Rodrigues <49556161+plucsa@users.noreply.github.com>
2023-09-30 17:50:51 -0300

    Delete projeto_pvp-main directory

--- projeto_pvp-main/production/views.py
+++
@@ -1,8 +0,0 @@
-# from django.shortcuts import render
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'pages/home.html', context={
-        'titulo': 'PVP S/A'
-    })
